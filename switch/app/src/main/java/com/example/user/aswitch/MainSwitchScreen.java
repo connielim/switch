1efd7f8233bb5b26434ec1c06c644dccc4881607
Connie Lim <conniel1@vt.edu>
2016-07-19 14:21:08 -0400

    Initial commit part 2
    Created an android studio project

---
+++ switch/app/src/main/java/com/example/user/aswitch/MainSwitchScreen.java
@@ -0,0 +1,13 @@
+package com.example.user.aswitch;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainSwitchScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main_switch_screen);
+    }
+}
