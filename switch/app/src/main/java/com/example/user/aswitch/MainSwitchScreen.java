130cb6283d673c8d1a314a59674dfc452d553125
Connie Lim <conniel1@vt.edu>
2016-07-21 13:07:54 -0400

    Added background switch
    The background of the screen will change depending on the status of the
    switch.  If the switch is on the left, then the light_on picture will
    sure.  If the switch is on the right, then the light_off picture will
    show.

--- switch/app/src/main/java/com/example/user/aswitch/MainSwitchScreen.java
+++ switch/app/src/main/java/com/example/user/aswitch/MainSwitchScreen.java
@@ -1,7 +1,12 @@
 package com.example.user.aswitch;
 
+import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.RelativeLayout;
+import android.widget.Switch;
 
 public class MainSwitchScreen extends AppCompatActivity {
 
@@ -9,5 +14,23 @@ public class MainSwitchScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_switch_screen);
+    }
+
+    public void changeBackground(View view){
+        Switch tempSwitch = (Switch) findViewById(R.id.switch1);
+        Resources res = getResources();
+        Drawable backgroundimg;
+        if(tempSwitch.isChecked()){
+            backgroundimg = res.getDrawable(R.drawable.light_off);
+        }
+        else{
+            backgroundimg = res.getDrawable(R.drawable.light_on);
+        }
+        setBackgroundImg(backgroundimg);
+    }
+
+    private void setBackgroundImg(Drawable src){
+        RelativeLayout mainSwitchScreen = (RelativeLayout) findViewById(R.id.mainScreen);
+        mainSwitchScreen.setBackground(src);
     }
 }
