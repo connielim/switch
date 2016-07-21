2bb4c10479516d70bc505e5fa2072bc9ad52f58b
Connie Lim <conniel1@vt.edu>
2016-07-21 17:10:59 -0400

    Added slide function
    Before, the image would change if you slide it, however, it can now!

--- switch/app/src/main/java/com/example/user/aswitch/MainSwitchScreen.java
+++ switch/app/src/main/java/com/example/user/aswitch/MainSwitchScreen.java
@@ -4,7 +4,9 @@ import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.view.View;
+import android.widget.CompoundButton;
 import android.widget.RelativeLayout;
 import android.widget.Switch;
 
@@ -14,7 +16,15 @@ public class MainSwitchScreen extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_switch_screen);
+        Switch tempSwitch = (Switch) findViewById(R.id.switch1);
+        final RelativeLayout mainSwitchScreen = (RelativeLayout) findViewById(R.id.mainScreen);
+        tempSwitch.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                changeBackground(mainSwitchScreen);
+            }
+        });
     }
+
 
     public void changeBackground(View view){
         Switch tempSwitch = (Switch) findViewById(R.id.switch1);
